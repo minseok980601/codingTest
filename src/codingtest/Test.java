8ac6752aa4c2ed42b4885e9b0d6729cc1a26bfbb
minseok980601 <minseok980601@gmail.com>
2022-10-06 02:39:55 +0900

    20221006 코딩테스트

--- src/codingtest/Test.java
+++ src/codingtest/Test.java
@@ -5,20 +5,47 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		int H = sc.nextInt();
-		int M = sc.nextInt();
+		int a = sc.nextInt();	// 시
+		int b = sc.nextInt();	// 분
+		int c = sc.nextInt();	// 추가할 분
 		
-		if(M < 45) {
-			H--;
-			M = 60 - (45 - M);
-			if(H < 0)
-				H = 23;
-			System.out.println(H + " " + M);
-		} else
-			System.out.println(H + " " + (M - 45));
+//		int bc = b + c;
 		
-
+		// 답
+		
+		while(b + c > 59) {
+			if(a == 23) {
+				a = 0;
+			} else {
+				a += 1;
+			}
+			c = c - 60;
+		}
+		System.out.println(a + " " + (b + c));
+		
+		// 오답(내가한것)
+		
+//		if (bc < 60) {
+//			System.out.println(a + " " + bc);
+//		} else if (bc > 59) {
+//			a += 1;
+//			bc = bc - 60;
+//			if (a > 23) {
+//				a = 0;
+//				System.out.println(a + " " + bc);
+//			}
+//			
+//			if (bc >= 60) {
+//				a += 1;
+//				bc = bc - 60;
+//				System.out.println(a + " " + bc);
+//			}
+//
+//		} 
+		
+		
 	}
 }
