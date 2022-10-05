b70305c33b031c0abfb40f08ecf62c7ce04f8653
minseok980601 <minseok980601@gmail.com>
2022-10-05 23:23:18 +0900

    20221005 복습

---
+++ src/codingtest/Repeat.java
@@ -0,0 +1,24 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class Repeat {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();		// 시
+		int b = sc.nextInt();		// 분
+		
+		if (b < 45) {
+			a--;
+			b = 60 - (45 - b);
+			if (a < 0)
+				a = 23;
+			System.out.println(a + " " + b);
+		} else
+			System.out.println(a + " " + (b - 45));
+	}
+
+}
